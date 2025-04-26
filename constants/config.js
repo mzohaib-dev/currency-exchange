1e47f202bbfff6b1a83039a85b31f387c99c47ca
Muhammad Zohaib <mzohaib.webdeveloper@gmail.com>
2025-04-26 16:39:42 +0400

    Add config and country list for currency exchange func

---
+++ constants/config.js
@@ -0,0 +1,9 @@
+const API_CONSTANTS = {
+  EXCHANGE_RATE_API: {
+    BASE_URL: "https://v6.exchangerate-api.com/v6",
+    KEY: "c6dd8f235d2d6fbe146d2d36",
+    ENDPOINTS: {
+      LATEST: "/latest",
+    },
+  },
+};
